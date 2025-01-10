5a136da609f6a251b4789960746ef70660d514bc
T43US <era18277@gmail.com>
2025-01-10 17:03:16 +0700

    +

---
+++ _nuxt/DuiGWzkR.js
@@ -0,0 +1,1 @@
+import{V as e}from"./CQrpUmtA.js";const a=e(_=>{_.path==="/"||_.path==="/ru/"||_.path==="/works"||_.path==="/ru/works"?document.body.classList.remove("light"):document.body.classList.add("light")});export{a as default};
