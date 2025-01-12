c4f65c29fabc62d1dd684964e46066b0ace551d7
T43US <era18277@gmail.com>
2025-01-12 17:23:41 +0700

    +

---
+++ _nuxt/BVtyFupi.js
@@ -0,0 +1,1 @@
+import{W as e}from"./DDNL1biG.js";const a=e(_=>{_.path==="/"||_.path==="/ru/"||_.path==="/works"||_.path==="/ru/works"?document.body.classList.remove("light"):document.body.classList.add("light")});export{a as default};
